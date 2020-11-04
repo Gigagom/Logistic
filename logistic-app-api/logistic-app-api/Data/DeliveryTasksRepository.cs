f70c76cac4cde879a0e74425d17f2413c08c0bba
Semyon <31039443+Gigagom@users.noreply.github.com>
2020-11-04 19:25:44 +0300

    add repositories for models

---
+++ logistic-app-api/logistic-app-api/Data/DeliveryTasksRepository.cs
@@ -0,0 +1,56 @@
+﻿using logistic_app_api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace logistic_app_api.Data
+{
+    public class DeliveryTasksRepository : IRepository<DeliveryTask>
+    {
+        private readonly LogisticContext _context;
+
+        public DeliveryTasksRepository(LogisticContext context)
+        {
+            _context = context;
+        }
+
+        public void Create(DeliveryTask item)
+        {
+            if(item == null)
+            {
+                throw new ArgumentNullException(nameof(item));
+            }
+            _context.DeliveryTasks.Add(item);
+        }
+
+        public void Delete(DeliveryTask item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException(nameof(item));
+            }
+            _context.DeliveryTasks.Remove(item);
+        }
+
+        public IEnumerable<DeliveryTask> GetAll()
+        {
+            return _context.DeliveryTasks.ToList();
+        }
+
+        public DeliveryTask GetById(int id)
+        {
+            return _context.DeliveryTasks.FirstOrDefault(c => c.Id == id);
+        }
+
+        public bool SaveChanges()
+        {
+            return (_context.SaveChanges() >= 0);
+        }
+
+        public void Update(DeliveryTask item)
+        {
+             //nothing
+        }
+    }
+}
---
+++ logistic-app-api/logistic-app-api/Data/UsersRepository.cs
@@ -0,0 +1,56 @@
+﻿using logistic_app_api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace logistic_app_api.Data
+{
+    public class UsersRepository : IRepository<User>
+    {
+        private readonly LogisticContext _context;
+
+        public UsersRepository(LogisticContext context)
+        {
+            _context = context;
+        }
+
+        public void Create(User item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException(nameof(item));
+            }
+            _context.Users.Add(item);
+        }
+
+        public void Delete(User item)
+        {
+            if (item == null)
+            {
+                throw new ArgumentNullException(nameof(item));
+            }
+            _context.Users.Remove(item);
+        }
+
+        public IEnumerable<User> GetAll()
+        {
+            return _context.Users.ToList();
+        }
+
+        public User GetById(int id)
+        {
+            return _context.Users.FirstOrDefault(t => t.Id == id);
+        }
+
+        public bool SaveChanges()
+        {
+            return (_context.SaveChanges() >= 0);
+        }
+
+        public void Update(User item)
+        {
+            //nothing
+        }
+    }
+}
